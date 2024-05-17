b07d23a2d832f69fd14ed793e6ead9ed250baab4
Sujal7ss <sujalchahande3@gmail.com>
2024-05-18 01:41:02 +0530

    Added Navbar

---
+++ Task2/getjob/src/pages/Homepage/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "../../components/Navbar/index";
+export default function Homepage() {
+  return <div>
+    <Navbar />
+  </div>;
+}
