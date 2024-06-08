3138e849f9ebec90999f088ef2f93c65d75da287
Sujal7ss <sujalchahande3@gmail.com>
2024-06-08 21:47:52 +0530

    c

--- Task2/client/src/pages/JobListing/index.jsx
+++ Task2/client/src/pages/JobListing/index.jsx
@@ -5,7 +5,7 @@ import JobsCard from "../../components/JobsCard";
 export default function JobListing({ JobList, applied,   user={user}}) {
   return (
     <>
-      <div className="job-listing flex flex-col md:w-3/4 items-center m-auto mt-7 md:gap-4 rounded-md">
+      <div className="job-listing flex flex-col w-3/4 items-center m-auto mt-7 gap-4">
         {JobList.map((Job) => (
           <JobsCard key={Job._id} job={Job} applied={applied}  user={user}/>
         ))}
